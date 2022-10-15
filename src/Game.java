215ead6a49d04fdb4acbf37eb4058b40f51eedde
mathias124 <mfs2001@hotmail.dk>
2022-10-15 16:53:03 +0200

    Setup

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
 public class Game {
+
+
+    boolean b = true;
+
+    Game() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (b) {
+
+            String hello = scanner.nextLine();
+
+        }
+    }
 }
+
+
+
+
