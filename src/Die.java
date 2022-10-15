5a7a4c04fd7c39ec0d884b240cc7cb2313f8b312
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2022-10-15 17:41:31 +0200

    sad

--- src/Die.java
+++ src/Die.java
@@ -1,2 +1,16 @@
 public class Die {
+    private final int MaxSides=6;
+    private int facevalue;
+
+    public Die(){
+        facevalue=6; // the state of the object before any action.
+    }
+
+    public int  roll(){
+        facevalue=(int)(Math.random()*MaxSides+1);
+        return facevalue;
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Holder tre;
+        tre = new Holder();
+        System.out.println(tre.sum());
 
     }
 }
