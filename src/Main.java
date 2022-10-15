0fee9772e19c1d16ff31f3a40ed8262d3074f1a9
mathias124 <mfs2001@hotmail.dk>
2022-10-15 19:30:09 +0200

    Square setup [data]

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-
+    Square square = new Square();
     Game game;
 
+
+        //System.out.println((square.location[8]));
+        System.out.println(square.location[8] + square.points[8]);
     }
+
 }
