03c16d4d6f88ecc24a6bb0d4150963336acaf784
mathias124 <mfs2001@hotmail.dk>
2022-10-19 17:14:42 +0200

    Update Of Die Class

--- src/Die.java
+++ src/Die.java
@@ -1,10 +1,10 @@
 public class Die {
 
-    private final int MaxSides=6;
+    private final int MaxSides = 6;
     private int facevalue;
 
-    public Die(){
-        facevalue=6; // the state of the object before any action.
+    public Die() {
+        facevalue = 6; // the state of the object before any action.
     }
 
     public int getFacevalue() {
@@ -15,25 +15,9 @@ public class Die {
         this.facevalue = facevalue;
     }
 
-    public int  roll(){
-        facevalue=(int)(Math.random()*MaxSides+1);
+    public int roll() {
+        facevalue = (int) (Math.random() * MaxSides + 1);
         return facevalue;
     }
-
-
-
-*/
-int die
-int sum;
-public Die(int sum){
-    this.sum = sum;
 }
-public void raffleCup(int dieSum){
-    sum = dieSum;
-    int size =6 ;
-    int die1 = (int) (Math.random()*size+1);
-    int die2 = (int) (Math.random()*size+1);
-    sum= die1 + die2;
-    }
-/*
-}
+
