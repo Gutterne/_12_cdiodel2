11704f490a789b31a2081c02e51a1d994a2f11da
mathias124 <mfs2001@hotmail.dk>
2022-10-19 16:59:46 +0200

    Merge Of MathiasBranch With Master after test.

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,18 +1,31 @@
 public class Wallet {
+
     private int money = 1000;
     Square square = new Square();
-    private String monetos = "";
     private int squareMoney;
     int[] SQpoints = square.points;
 
     public Wallet() {
     this.money = money;
 
-
     }
+    public void setSquareMoney(int SquareMoney) {
+        squareMoney = SquareMoney;
+    }
+
     public int GetMoney() {
         //1000 + 60 for testing = 1060 ;D
         int squareMoney = money + SQpoints[9];
         return squareMoney;
     }
-}
+    public int DecreasMoney() {
+        if (squareMoney > money) {
+            money = 0;}
+                else {
+                    money = money - squareMoney;
+                }
+    return squareMoney;
+    }
+
+    }
+
