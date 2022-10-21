ee9b348a8dd16f7f53d001e5dca2026162d7f801
Marble <62856659+Marble5@users.noreply.github.com>
2022-10-21 11:30:14 +0200

    Laver Kommentare

--- src/Square.java
+++ src/Square.java
@@ -2,8 +2,14 @@ public class Square {
 
     String[] location = {"kendt", " ", "Tower", "Crater", "Palace gates", "Cold Dessert", "Walled City", "Monastery", "Black Cave", "Huts in the mountain", "The Werewall", "The pit", "Goldmine"};
     int[] points = {0, 0, 250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
+
+    //Array Kommentare
+    String[] negativkommentare={"Det gik ikke så godt!","Du igang med at gå bankerot","Nårh, det var ikke så godt","Du blir bare fattigere og fattigere","Du længere væk fra at blive min sugardaddy","Det gik sku nedaf"};
+
+
     Square() {
     }
     public void update(){
+
     }
 }
