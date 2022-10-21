62de32e3affc9ec59759def24facd7b6fedd0cc0
Marble <62856659+Marble5@users.noreply.github.com>
2022-10-21 13:03:41 +0200

    Creation of number loop, comment method with reference to both positive and negative comment arrays.

--- src/Square.java
+++ src/Square.java
@@ -5,7 +5,7 @@ public class Square {
 
     //Array Kommentare
     String[] negativeComments={"Det gik ikke så godt!","Du igang med at gå bankerot","Nårh, det var ikke så godt","Du blir bare fattigere og fattigere","Du længere væk fra at blive min sugardaddy","Det gik sku nedaf","Du lort, bli bedre noob!","gg, er noget du ikke burde sige"};
-    String[] positivComments={"Det går flyvende","du sindsyg!","Har du det varmt, eller er det kun mig?","Hvordan er du så god!","Du må gerne blive min sugardaddy","DET KAN IKKE BLIVE BEDRE!","SINDSSYYGGTT!!","LETTSS GOOOOOOOOOO"};
+    String[] positiveComments={"Det går flyvende","du sindsyg!","Har du det varmt, eller er det kun mig?","Hvordan er du så god!","Du må gerne blive min sugardaddy","DET KAN IKKE BLIVE BEDRE!","SINDSSYYGGTT!!","LETTSS GOOOOOOOOOO"};
 
     Square() {
     }
