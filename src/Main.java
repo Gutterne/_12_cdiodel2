fa5585f39713a993d46a1e353bec894a2b7f35c8
Marble <62856659+Marble5@users.noreply.github.com>
2022-10-24 14:24:09 +0200

    Kommentare

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
     Game game=new Game();
 
-
+        /**
+         * Referer til game.play() som er et true loop, der sørger for at spillet løber indtil der er vundet.
+         */
     game.play();
 
     }
