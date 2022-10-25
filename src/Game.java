47feedbe12836ff8c22924f84b765af6429006d9
Marble <62856659+Marble5@users.noreply.github.com>
2022-10-25 15:28:26 +0200

    Fix af output

--- src/Game.java
+++ src/Game.java
@@ -5,17 +5,15 @@ public class Game {
 
     int randomNum;
     Wallet wallet;
-    Player player1,player2;
+    Player player1, player2;
     Square square;
 
     Scanner scanner = new Scanner(System.in);
 
 
-
-
     boolean playing = true;
 
-    public Game(){
+    public Game() {
         holder = new Holder();
 
         player1 = new Player();
@@ -36,25 +34,25 @@ public class Game {
         while (playing) {
 
             updateFlow();
-            while (holder.getSum()==10){
+            while (holder.getSum() == 10) {
                 System.out.println("Du får et ekstra slag");
                 updateFlow();
 
             }
-            if(player1.myWallet.getMoney()>=3000){
-                playing=false;
+            if (player1.myWallet.getMoney() >= 3000) {
+                playing = false;
                 System.out.println("Spiller 1 har vundet");
                 break;
             }
 
             updateFlow2();
-            while(holder.getSum()==10){
+            while (holder.getSum() == 10) {
                 System.out.println("Du får et ekstra slag");
                 updateFlow2();
 
             }
-            if(player2.myWallet.getMoney()>=3000){
-                playing=false;
+            if (player2.myWallet.getMoney() >= 3000) {
+                playing = false;
                 System.out.println("Spiller 2 har vundet");
                 break;
             }
@@ -62,22 +60,9 @@ public class Game {
 
     }
 
-        while(playing){
-
-        //Player 1
-            ////////////////////////////// TESTING
 
 
-        updateFlow();
-            //System.out.println(wallet.SQpoints[8]);
-            ////////////////////////////////////
-        //Player 2
 
-        updateFlow2();
-        updateNum(); // Update of number in a loop
-        }
-
-        }
 
 
 
@@ -88,7 +73,7 @@ public class Game {
         System.out.println("Spiller 1 du slog " + holder.getSum() + " og landet på "+ square.location[holder.getSum()] + " Denne Square Værdi gav : " + square.points[holder.getSum()]);
         player1.myWallet.setSquareMoney(square.points[holder.getSum()]);
         Comment();
-        System.out.print(" player 1 har nu "+player1.myWallet.UpdateMoney()+" ");
+        System.out.println(" player 1 har nu "+player1.myWallet.UpdateMoney()+" ");
     }
 
     public void updateFlow2(){
@@ -99,7 +84,7 @@ public class Game {
         System.out.println("Spiller 2 du slog " + holder.getSum() + " og landet på "+ square.location[holder.getSum()] + " Denne Square Værdi gav : " + square.points[holder.getSum()]);
         player2.myWallet.setSquareMoney(square.points[holder.getSum()]);
         Comment();
-        System.out.print(" player 2 har nu "+player2.myWallet.UpdateMoney()+" ");
+        System.out.println(" player 2 har nu "+player2.myWallet.UpdateMoney()+" ");
 
     }
 
